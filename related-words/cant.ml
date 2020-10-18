d2e95a741f32f634fde6863e51809336d522f5b0
Teemu Rönty <teemu.ronty@student.oulu.fi>
2020-10-18 18:23:33 +0300

    Necessary commitment

---
+++ related-words/cant.ml
@@ -0,0 +1,25 @@
+inclined
+lingo
+slang
+jargon
+bevel
+beveled
+patois
+vernacular
+argot
+camber
+buzzword
+slant
+tilt
+bank
+pitch
+camfer
+cant over
+pious platitude
+whine
+nb
+slope
+methodistical
+whats
+lol
+cuz
