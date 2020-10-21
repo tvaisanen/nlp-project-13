be000601338eac47d488a84c7c4879a0edd8d1fb
tvaisanen <vaisanen.toni@gmail.com>
2020-10-21 19:27:04 +0300

    gui app base

--- demo-app/server/server.py
+++ demo-app/server/server.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, request, send_from_directory, jsonify
+from os import path, listdir
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return "helo"
+
+@app.route('/words')
+def get_available_words():
+    d =  listdir('../../related-words')
+    d.sort()
+    return  jsonify([w.replace('.ml', '') for w in d])
+
+@app.route('/ml/<string:word>')
+def get_ml_word(word):
+    with open(path.join('../../related-words', "{}.ml".format(word))) as f:
+       return jsonify(f.read().split('\n'))
+
 
 if '__main__' == __name__:
     app.run(debug=True)
--- demo-app/web/src/App.tsx
+++ demo-app/web/src/App.tsx
@@ -1,21 +1,70 @@
 import React from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [state, setState] = React.useReducer(
+    (a: any, b: any) => ({ ...a, ...b }),
+    { r: "", right: [], l: "", left: [], union: [] }
+  );
+
+  React.useEffect(() => {
+    axios.get("/words").then(({ data }) => setState({ words: data }));
+  }, []);
+
+  const loadRelated = (w: string, l: string) => {
+    const lr = l === "left" ? "l" : "r";
+    axios.get(`/ml/${w}`).then(({ data }) => setState({ [l]: data, [lr]: w }));
+  };
+
+  React.useEffect(() => {
+    const s = [...state.right, ...state.left].filter(
+      (w) => state.left.includes(w) && state.right.includes(w) && w !== ""
+    );
+    console.log(s);
+    setState({ union: Array.from(new Set(s)) });
+  }, [state.right, state.left]);
+
+  console.log(state);
   return (
     <div className="App">
-      <div>
-        <input value={"lause paikoillaan"} />
-        <select>
-          <option>left</option>
-        </select>
-        <input value={"lause paikoillaan"} />
-        <select>
-          <option>right</option>
-        </select>
+      <div className="block">
+        <div className="select">
+          <select onChange={(e) => loadRelated(e.target.value, "left")}>
+            <option>Select a word</option>
+            {state.words &&
+              state.words.map((w: string) => <option value={w}>{w}</option>)}
+          </select>
+        </div>
+        <div className="select">
+          <select onChange={(e) => loadRelated(e.target.value, "right")}>
+            <option>Select a word</option>
+            {state.words &&
+              state.words.map((w: string) => <option value={w}>{w}</option>)}
+          </select>
+        </div>
+      </div>
+      <div className="columns">
+        <div className="column is-4">
+          <div className="subtitle is-2">{state.l}</div>
+          {state.left &&
+            state.left.map((w: string, i: number) => <div key={i}>{w}</div>)}
+        </div>
+        <div className="column is-4">
+          <div className="subtitle is-2">Common</div>
+          {state.union &&
+            state.union.map((w: string, i: number) => <div key={i}>{w}</div>)}
+        </div>
+        <div className="column is-4">
+          <div className="subtitle is-2">{state.r}</div>
+          {state.right &&
+            state.right.map((w: string, i: number) => <div key={i}>{w}</div>)}
+        </div>
       </div>
     </div>
   );
 }
 
+// <input value={"lause paikoillaan"} />
+// <input value={"lause paikoillaan"} />
 export default App;
