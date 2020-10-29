f63eaaa1c2889c2387fb6613e434c5a67dae33eb
tvaisanen <vaisanen.toni@gmail.com>
2020-10-29 18:56:46 +0200

    server data endpoints

--- demo-app/server/server.py
+++ demo-app/server/server.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, jsonify
+import csv
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "helo"
+@app.route('/data/stss131/sentences')
+def sentences():
+    with open('data/stss.csv', 'r') as file:
+        return jsonify([ 
+            (p[1], p[2], float(p[3])) for p in [
+            line.replace('\n', '').split(';') for 
+            line in 
+            file
+        ]]) 
+
+
+@app.route('/data/stss131/similarity')
+def foo():
+    with open('data/similarity_scores.csv', 'r') as file:
+        file_data = [
+            line.replace('\n', '').split(',') for 
+            line in 
+            file]
+        return jsonify(dict(
+            labels=file_data[0],
+            data=file_data[1:]
+        ))
+
 
 if '__main__' == __name__:
     app.run(debug=True)
